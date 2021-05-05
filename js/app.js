fde380d9e8e7b7db4bfd79e8bef3788523074cb0
Norton Pengra <npengra@nvidia.com>
2021-05-04 20:17:49 -0700

    wrap q1 in a function

--- js/app.js
+++ js/app.js
@@ -6,13 +6,16 @@ let username = prompt('What is your name?');
 
 alert('Welcome ' + username + '!  Please answer the following five questions yes/no or y/n');
 
-let answerOne = prompt('Do I live in Seattle?').toLowerCase();
-// this is required
-if(answerOne === 'yes' || answerOne === 'y'){
-  // console.log('answerOne: ' + answerOne);
-  alert('you are correct!  I live in Seattle');
+function q1() {
+  let answerOne = prompt('Do I live in Seattle?').toLowerCase();
+  // this is required
+  if(answerOne === 'yes' || answerOne === 'y'){
+    // console.log('answerOne: ' + answerOne);
+    alert('you are correct!  I live in Seattle');
+  }
 }
 
+q1();
 
 // this is doing extra
 // if(answerOne === 'yes' || answerOne === 'y'){
